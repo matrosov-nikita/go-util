61f55c401a970e9c3f1a53dcafb26a27a4c9511c
i-timofeev <ilya.timofeev@ru.axxonsoft.com>
2019-11-29 14:39:53 +0300

    added logs

--- log.go
+++ log.go
@@ -3,6 +3,7 @@ package util
 import (
 	"github.com/sirupsen/logrus"
 	"os"
+	"time"
 )
 
 const LogTimestampFormat = "2006-01-02T15:04:05.999"
@@ -20,4 +21,27 @@ func LogWithError(log logrus.FieldLogger, err error, msg string) {
 		return
 	}
 	log.Debug(msg)
+}
+
+// LogStartStop logs start and finish of application
+// correct way to call is the following:
+//
+// defer util.LogStartStop(config.Log, config.Version, "arp server") ()
+func LogStartStop(log logrus.FieldLogger, version interface{}, appName string) func() {
+	entry := log.WithFields(logrus.Fields{
+		"version": version,
+		"app":     appName,
+	})
+	entry.Debug("application starting")
+
+	return func() {
+		err := recover()
+		if err != nil {
+			entry.WithField("err", err).Error("exited with error")
+			panic(err)
+		} else {
+			entry.Debug("exit with ok")
+		}
+		time.Sleep(time.Millisecond)
+	}
 }
